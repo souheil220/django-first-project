590db06901b67628109b9098ae19b4391032b07b
souhil433 <souhil433@gmail.com>
2020-11-02 11:26:29 +0100

    Display & Search

--- realtors/admin.py
+++ realtors/admin.py
@@ -4,9 +4,10 @@ from .models import Realtor
 
 
 class RealtorAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "email", "hire_date")
+    list_display = ("id", "is_mvp", "name", "email", "hire_date")
     list_display_links = ("id", "name")
     search_fields = ("name",)
+    list_editable = ("is_mvp",)
     list_per_page = 25
 
 
